f5cffbf99b50e8b9bb63a83627ddcd61f716fb01
Annie <56055607+annie951977@users.noreply.github.com>
2020-06-20 22:15:24 -0400

    working on the mostTradedWithUser method. It's a pain in my ass

--- phase1/src/TradeHistory.java
+++ phase1/src/TradeHistory.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Hashtable;
+import java.util.Set;
+import java.util.Iterator;
 
 /**
  * Represents the trade history of a User
@@ -29,12 +30,39 @@ public class TradeHistory {
     /**
      * Returns the users top three trading partners
      */
-    public ArrayList<User> mostTradedWithUsers(){
+    public ArrayList<String> mostTradedWithUsers(){
         HashMap<User, Integer> temp = (HashMap<User, Integer>) usersNumTradeTimes.clone();
         ArrayList<User> mostTradedWith = new ArrayList<>();
-
+        ArrayList<String> mostTradedWithUsernames = new ArrayList<>();
         // TODO: Figure out how this top three situation is going to work
+        // fuck it, let's just iterate through multiple times because I dont want to deal with a linked hashmap jfc
 
+        // if the user hasn't traded with anyone yet
+        if(usersNumTradeTimes.size() == 0){
+            String note = new String("You haven't traded with anyone yet!");
+            mostTradedWithUsernames.add(note);
+            return mostTradedWithUsernames;
+        }
+        // if user has less than or equal to 3 trades
+        if(usersNumTradeTimes.size() <= 3){
+            Set<User> users = usersNumTradeTimes.keySet();
+            for (User userTemp : users) {
+                mostTradedWithUsernames.add(userTemp.getUsername());
+            }
+            return mostTradedWithUsernames;
+            }
+
+
+        // if user has greater than 3 trades
+        int j = 0;
+        while(j < 3){
+            ArrayList<HashMap<User, Integer>> max = new ArrayList<>();
+            for(User i: temp.keySet()){
+                if (temp.get(i) < max[0].value(){
+
+            }
+            j ++;
+        }
 
 
     }
