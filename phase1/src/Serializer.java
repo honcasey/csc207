eda722f2dd0655cfe04a7990fdada5ce943bd366
brandontiu <tiubrandon@hotmail.com>
2020-06-21 19:55:35 -0400

    fixed Serializer.java

--- phase1/src/Serializer.java
+++ phase1/src/Serializer.java
@@ -3,12 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 // general ideas taken from lecture 6 StudentManager.java example
-public class UserSerializer {
+public class Serializer {
     List<User> users;
+    List<AdminUser> admins;
 
     // cited from https://docs.oracle.com/javase/7/docs/api/java/io/ObjectOutputStream.html
 
-    public void writeToFile(String path) throws IOException {
+    public void writeUsersToFile(String path) throws IOException {
 
         // create a connection to the file specified by path
         OutputStream file = new FileOutputStream(path);
@@ -21,9 +22,23 @@ public class UserSerializer {
         output.close();
     }
 
+    public void writeAdminsToFile(String path) throws IOException {
+
+        // create a connection to the file specified by path
+        OutputStream file = new FileOutputStream(path);
+        ObjectOutput output = new ObjectOutputStream(file);
+
+        // write user into the file
+        output.writeObject(admins);
+
+        // close the file
+        output.close();
+    }
+    }
+
     // cited from here https://docs.oracle.com/javase/7/docs/api/java/io/ObjectInputStream.html
 
-    public void readFromFile(String path) {
+    public void readUsersFromFile(String path) {
         try {
             // create a connection to the file specified by path
             InputStream file = new FileInputStream(path);
@@ -40,7 +55,37 @@ public class UserSerializer {
 
     }
 
+    public void readAdminsFromFile(String path) {
+        try {
+            // create a connection to the file specified by path
+            InputStream file = new FileInputStream(path);
+            ObjectInput input = new ObjectInputStream(file);
+
+            // deserialize the list
+            admins = (List<AdminUser>) input.readObject();
+
+            // close the file
+            input.close();
+        } catch (IOException | ClassNotFoundException e) {
+            // TODO idk what we should return if an error is caught
+        }
+
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+
     public List<User> getUsers() {
         return users;
+    }
+
+    public void setAdmins(ArrayList<AdminUser> admins) {
+        this.admins = admins;
+    }
+
+    public List<AdminUser> getAdmins() {
+        return admins;
     }
 }
