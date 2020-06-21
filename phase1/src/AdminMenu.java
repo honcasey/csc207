88c0b118f3ba2aff94a034ea14eddb426984d5b4
Casey Hon <casey.hon@mail.utoronto.ca>
2020-06-21 19:06:14 -0400

    created AdminMenu controller, changed User threshold name to borrowThreshold, added getters and setters to new thresholds, changed tradeHistory to transactionHistory

---
+++ phase1/src/AdminMenu.java
@@ -0,0 +1,8 @@
+/**
+ * Controls the system responsible for AdminUsers and an Administrative User's abilities in the trading system.
+ */
+public class AdminMenu {
+    private AdminUser currentAdmin;
+
+
+}
