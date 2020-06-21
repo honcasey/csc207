04c745f34567f9141399b4f335cae875154cc652
Anna Shirkalina <ashirkalina@gmail.com>
2020-06-21 16:13:12 -0400

    create TransactionManager

--- phase1/src/TransactionManager.java
+++ phase1/src/TransactionManager.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+
 /**
  * This class manages a transaction between two users. A transaction begins once a user expresses interest in an item.
  */
 public class TransactionManager {
-
+    private List<Transaction> allTransactions = new ArrayList<>();
     /**
      * Checks if this transaction can be initiated based on the status of the users
      * A transaction is valid if both users accounts are not frozen,
@@ -16,17 +20,40 @@ public class TransactionManager {
 
     }
     /**
-     * Return true if a one-way transaction was create, false otherwise
-     * A transaction will be created only if
-     * @param threshold as an integer
-     *
+     * @return one-way transaction
+     * A transaction will be created only if it is valid, otherwise an error will be thrown     *
+     * @param user1 the user who has the item.
+     * @param user2 the user who want to borrow an item.
+     * @param item1 the item that user2 wants to borrow.
+     * @param type must be either "temp" or "perm", specifies what type of transaction will be created
+     * @param location1 the first meeting location that the users will meet at to exchange items.
+     * @param location2 the second meeting location that the users will meet at to exchange items.
      */
-    public boolean createOneWayTransaction(User user1, User user2, Item item1, String type){
-        if not transactionIsValid(user1, user2, "oneway")
-        if (type.equals("temp")){
-            Transaction transaction = TransactionOneWayTemp(user1, user2);
+
+
+    public Transaction createOneWayTransaction(User user1, User user2, Item item1, String type, LocalDate date1,
+                                           LocalDate date2) throws Exception {
+        if (! transactionIsValid(user1, user2, "oneway")){
+            throw new Exception("the transaction is not valid");
+        }
+        if (type.equals("temp")) {
+            Transaction transaction = new TransactionOneWayTemp(user1, user2, item1, date1, date2);
+            allTransactions.add(transaction);
         }
     }
+
+    public boolean createOneWayTransaction(User user1, User user2, Item item1, String type, LocalDate date1) {
+        if (! transactionIsValid(user1, user2, "twoway")){
+            throw exception("the transaction is not valid");
+        }
+        if (type.equals("perm")){
+            Transaction transaction = new TransactionOneWayTemp(user1, user2, item1, date1);
+            allTransactions.add(transaction);
+            return transaction
+        }
+    }
+
+
 
     public void createTwoWayTransaction(User user1, User user2, Item item1, Item item2, String type){
 
