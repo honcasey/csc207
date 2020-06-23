042b7d02575ec752d9f004bd0f5d28e82ae248d7
Anna Shirkalina <ashirkalina@gmail.com>
2020-06-23 18:24:42 -0400

    added a user1approved and user2approved getters and setters in Meeting

--- phase1/src/Meeting.java
+++ phase1/src/Meeting.java
@@ -19,9 +19,13 @@ public class Meeting {
     LocalDate getDate(){return date;}
     LocalTime getTime(){return time;}
     String getLocation(){return location;}
-    
+    Boolean getUser1approved(){return user1approved;}
+    Boolean getUser2approved(){return user2approved;}
+
     void setLocation(String newLocation){location = newLocation;}
     void setDate(int year, int month, int dayOfMonth){this.date = LocalDate.of(year, month, dayOfMonth);}
     void setTime(int hour, int minutes){this.time = LocalTime.of(hour, minutes);}
+    void setUser1approved(Boolean bool){user1approved = bool;}
+    void setUser2approved(Boolean bool){user2approved = bool;}
 }
 
