1d7da0686bfe755159773327c582584796e3df14
Christian Mitrache <christian.mitrache@mail.utoronto.ca>
2020-06-23 22:39:37 -0400

    Added UserMenu class

---
+++ phase1/src/UserMenu.java
@@ -0,0 +1,12 @@
+public class UserMenu {
+    User currentUser;
+
+    public UserMenu(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public String requestAddItem(String itemName){
+
+    }
+
+}
