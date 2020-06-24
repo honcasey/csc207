e465adbcadcf82a8db08e62f51f03e1b719e690f
zhan4100 <ztt.zhang@mail.utoronto.ca>
2020-06-24 00:20:36 -0400

    add withdrawItem, addToWishlist method and javadoc in UserMenu

--- phase1/src/UserMenu.java
+++ phase1/src/UserMenu.java
@@ -1,3 +1,5 @@
+import java.util.UUID;
+
 public class UserMenu {
     private User currentUser;
     private AdminManager am;
@@ -11,6 +13,27 @@ public class UserMenu {
 
     public String requestAddItem(String itemName){
 
+    }
+
+    /**
+     * To withdraw item from user's specified list, which is either the User's wishlist or inventory.
+     * @param itemId the id of an item
+     * @param listType either "wishlist" or "inventory" as a String
+     */
+    public void withdrawItem(UUID itemId, String listType){
+        if(listType.equals("wishlist")){
+            um.removeItem(currentUser, itemId, "wishlist");
+        }else if (listType.equals("inventory")){
+            um.removeItem(currentUser,itemId,"inventory");
+        }
+    }
+
+    /**
+     * To add an given item to user's wishlist
+     * @param itemId the id of an item
+     */
+    public void addToWishlist(UUID itemId){
+        um.addItem(currentUser.getUsername(), itemId, "wishlist");
     }
 
 }
