7cf421b607704b8c7a3abb42e0f49c46d0223dfc
Christian Mitrache <christian.mitrache@mail.utoronto.ca>
2020-06-26 17:31:11 -0400

    Completed meeting class, added documentation, changed data structures and re-implemented getters and setters so they remain the same.
     Added method that changes details of meeting by string. (Only to be called by the transaction class).
    
     Added abstract method in transction class that changes transaction details by string.(need to implement the method in the subclasses still)

--- phase1/src/Meeting.java
+++ phase1/src/Meeting.java
@@ -1,34 +1,133 @@
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-public class Meeting {
-    String location;
-    LocalDate date;
-    LocalTime time;
-    Boolean user1approved = Boolean.FALSE;
-    Boolean user2approved = Boolean.TRUE;
-    int numEdits = 0;
+import java.util.HashMap;
 
+
+public class Meeting {
+    /**
+     * This class represents a meeting among 2 users.
+     * Variables:
+     *
+     * user1approved: Keeps track of whether the user has confirmed the details of the meeting.
+     *
+     * user2approved: explained above.
+     *
+     * numedits: this stores the number of times that both users has made an edit to the program (check phase 1
+     * if you disagree.) example: user1 and user2 need to both edit this meeting in order for numedits to increase to 1
+     * from 0.
+     *
+     * userEditable: This is a mapping of all the information that the user can change. This contains capitalized
+     * strings mapping to the information they change.
+     *
+     * NOTE: the following variables are considered USER EDITABLE: Time, Date, Location. (will be stored in the hashmap)
+     * NOTE: getters and setters for certain information still remain camel-case.
+     */
+    private Boolean user1approved = Boolean.FALSE;
+    private Boolean user2approved = Boolean.TRUE;
+    private int numEdits = 0;
+    private HashMap<String,Object> userEditable;
+
+    /**
+     * The constructor for the class Meeting.
+     * @param location location to be stored in usereditable hashmap.
+     * @param year The year for the date of the meeting.
+     * @param month the month for the date of the meeting
+     * @param dayOfMonth the day of the month for the meeting.
+     * @param hour the hour for the meeting.
+     * @param minutes the minute for the meeting.
+     */
 
     Meeting(String location, int year, int month, int dayOfMonth, int hour, int minutes){
-        this.location = location;
-        this.date = LocalDate.of(year, month, dayOfMonth);
-        this.time = LocalTime.of(hour, minutes);
-
+        this.userEditable.put("Location",location);
+        this.userEditable.put("Time",LocalTime.of(hour, minutes));
+        this.userEditable.put("Date", LocalTime.of(hour, minutes));
     }
 
-    LocalDate getDate(){return date;}
-    LocalTime getTime(){return time;}
-    String getLocation(){return location;}
-    Boolean getUser1approved(){return user1approved;}
-    Boolean getUser2approved(){return user2approved;}
-    int getNumEdits(){return numEdits;}
+    /**
+     * Getter for if user1 has approved meeting details
+     * @return returns true iff user has approved meeting details.
+     */
+    public Boolean getUser1approved(){return user1approved;}
 
-    void setLocation(String newLocation){location = newLocation;}
-    void setDate(int year, int month, int dayOfMonth){this.date = LocalDate.of(year, month, dayOfMonth);}
-    void setTime(int hour, int minutes){this.time = LocalTime.of(hour, minutes);}
-    void setUser1approved(Boolean bool){user1approved = bool;}
-    void setUser2approved(Boolean bool){user2approved = bool;}
-    void setNumEdits(int num){numEdits = num;}
+    /**
+     * Getter for if user2 has approved meeting details
+     * @return returns true iff user2 has approved meeting details
+     */
+
+    public Boolean getUser2approved(){return user2approved;}
+
+    /**
+     * Getter for number of edits for this particular meeting.
+     * @return returns the number of times both users have edited a meeting.
+     */
+    public int getNumEdits(){return numEdits;}
+
+    /**
+     * This method is a helper method that lets usecase classes edit user-editable information.
+     * DO NOT USE THIS AS A GETTER(if you have any tips on how to make this so only certain classes can access this
+     * that would be great.)
+     * @param FieldString The field that needs to be changed.
+     * @param NewVal the new value corresponding the field above.
+     * @return returns true iff method finds information in hashmap and changes it successfully.
+     */
+    protected Boolean userChangeByString(String FieldString, Object NewVal){
+        if(this.userEditable.containsKey(FieldString)) {
+            this.userEditable.put(FieldString, NewVal);
+            return(true);
+        }
+        else{
+            return(false);
+        }
+    }
+
+    /**
+     * This is a setter for changing a new location. This u
+     * @param newLocation the new location.
+     */
+
+    public void setLocation(String newLocation){
+        this.userEditable.put("Location",newLocation);
+    }
+
+
+    /**
+     * This is a setter for the date of the transaction.
+     * @param year year of the date to be set.
+     * @param month month of the date to be set.
+     * @param dayOfMonth day of the month to be set.
+     */
+    public void setDate(int year, int month, int dayOfMonth) {
+        this.userEditable.put("Date", LocalDate.of(year, month, dayOfMonth));
+    }
+
+    /**
+     * This is a setter for the time of the transaction.
+     * @param hour the hour of the time to be set.
+     * @param minutes the minute of the time to be set.
+     */
+
+    public void setTime(int hour, int minutes){
+        this.userEditable.put("Time",LocalTime.of(hour, minutes));
+    }
+
+    /**
+     * This is setter for user1 status on meeting details.
+     * @param bool new confirmation status.
+     */
+    public void setUser1approved(Boolean bool){this.user1approved = bool;}
+
+    /**
+     * this is a setter for user2 status on meeting details.
+     * @param bool new confirmation status.
+     */
+    public void setUser2approved(Boolean bool){this.user2approved = bool;}
+
+    /**
+     * this is the setter for the numEdits variable defined in the beggining of the documentation for this class.
+     * @param num new value for numedits variable.
+     */
+    public void setNumEdits(int num){this.numEdits = num;}
+
 }
 
