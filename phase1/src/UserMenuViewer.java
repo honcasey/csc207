7d132b73506d8d1eeea0c49dce43fa646c7a06a7
Christian Mitrache <christian.mitrache@mail.utoronto.ca>
2020-06-29 18:45:18 -0400

    Almost finished DisplayAvailableItems flow.
    Made new helper method HandleOptions which handles any options.
    
    Started CreateTransactions, MeetingDetailsMenu flow and have several questions about how to deal with changes that will occur to information needed for MeetingDetailsMenu.

--- phase1/src/UserMenuViewer.java
+++ phase1/src/UserMenuViewer.java
@@ -1,6 +1,5 @@
-import java.util.List;
-import java.util.Scanner;
-import java.util.Iterator;
+import javax.swing.text.html.Option;
+import java.util.*;
 
 public class UserMenuViewer {
     private UserMenu userMenu;
@@ -9,11 +8,54 @@ public class UserMenuViewer {
     public UserMenuViewer(UserMenu userMenu) {
         this.userMenu = userMenu;
     }
+
+    private int HandleOptions(Scanner scanner, List<String> OptionList, boolean BackOption, String OptionPrompt){
+        for(int i = 0; i < OptionList.size(); i++){
+            String index = Integer.toString(i+1);
+            String OutputLine =  index + ". " + OptionList.get(i);
+            System.out.println(OutputLine);
+        }
+        if(BackOption) {
+            String LastIndex = Integer.toString(OptionList.size() + 1);
+            String LastOption = ". Go back";
+            System.out.println(LastIndex + LastOption);
+
+            int OptionChosen;
+            do {
+                System.out.println(OptionPrompt);
+                while (!scanner.hasNextInt()) {
+                    System.out.println("That is not a valid option." +
+                            " Please enter a number corresponding to one of the options.");
+                    scanner.next();
+                }
+                OptionChosen = scanner.nextInt();
+            } while (OptionChosen > OptionList.size() + 1 || OptionChosen <= 0);
+            return (OptionChosen);
+        }
+        else{
+            int OptionChosen;
+            do {
+                System.out.println(OptionPrompt);
+                while (!scanner.hasNextInt()) {
+                    System.out.println("That is not a valid option." +
+                            "Please enter a number corresponding to one of the options.");
+                    scanner.next();
+                }
+                OptionChosen = scanner.nextInt();
+            } while (OptionChosen > OptionList.size() || OptionChosen <= 0);
+            return (OptionChosen);
+        }
+    }
+
+
     public void run() {
         Scanner scanner = new Scanner(System.in);
         boolean UserMenuActivity = true;
 
         while (UserMenuActivity) {
+            System.out.println("Main Menu");
+            System.out.println("---------");
+
             System.out.println("1. Request Item for Approval");
             System.out.println("2. Browse Available Items for Trade");
             System.out.println("3. View Active Transactions");
@@ -22,13 +64,13 @@ public class UserMenuViewer {
             System.out.println("6. View Inventory");
             System.out.println("7. Log Out");
 
-            System.out.println("Pick an option.");
+            System.out.println("Pick an option from above.");
             input = scanner.nextInt();
 
             if (input == 1) {
                 this.requestAddItem(scanner);
             } else if (input == 2) {
-                // this.getAvailableItemsFlow(scanner);
+                 this.DisplayAvailableItems(scanner);
             } else if (input == 3) {
                 // call this.um.getActiveTransactions()
             } else if (input == 4) {
@@ -59,31 +101,105 @@ public class UserMenuViewer {
         System.out.println("Item has been requested and is now being reviewed by the administrators.");
     }
 
+    /**
+     * NOT FINISHED! (Almost finished though)
+     * This is the method for handling the flow of:
+     * 1) Displaying all available items to trade.
+     * 2) Allowing the user to go to an item.
+     * 3) Check if the transaction can occur.
+     *
+     * QUESTION: ARE THE AVAILABLE ITEMS FROM OTHER USERS WHO CAN TRADE? DO WE CHECK BEFORE ADDING TO AVAILABLE ITEMS
+     * OR DO WE CHECK ONCE THE USER HAS SELECTED AN ITEM?
+     *
+     * @param scanner the scanner constructed from the run method.
+     */
     private void DisplayAvailableItems(Scanner scanner){
-        String ItemOutputName = ") Item Name:";
-        String ItemOutputDescription = " ||  Item Description:";
-        List<Item> AvailableItems = this.userMenu.getAvailableItems();
-        for (int i = 0; i < AvailableItems.size(); i++){
-            String index = Integer.toString(i);
-            String OutputLine =  index + ItemOutputName + AvailableItems.get(i).getName() +
-                    ItemOutputDescription+AvailableItems.get(i).getDescription();
-            System.out.println(ItemOutputDescription);
+
+        System.out.println("Available Items for Trade:");
+        System.out.println("--------------------------");
+        String ItemOutputName = ") Item Name: ";
+        String ItemOutputDescription = " |  Item Description: ";
+        HashMap<Item, User> AvailableItems  = this.userMenu.getAvailableItems();
+        List<Item> ItemList = new ArrayList<>(AvailableItems.keySet());
+        // Making Option List
+
+        List<String> OptionList = new ArrayList<>();
+        for (Item item : ItemList) {
+            String OutputLine = ItemOutputName + item.getName() +
+                    ItemOutputDescription + item.getDescription();
+            OptionList.add(OutputLine);
         }
-        String LastIndex = Integer.toString(AvailableItems.size()+ 1);
-        String LastOption =") Go back";
-        System.out.println(LastIndex+LastOption);
+        String AvailableItemsPrompt = "Type the number corresponding to the item you wish to" +
+                " create transaction for. To go back to the previous menu, type the number corresponding to that" +
+                "option.";
+
+        int OptionChosen = this.HandleOptions(scanner,OptionList,true,AvailableItemsPrompt);
+        // Logic handling back to other menu vs. Proceed to make transaction.
+        if(OptionChosen > OptionList.size()){
+            System.out.println("Loading Previous Menu");
+            System.out.println(".........");
+        }
+        else{
+            Item TransactionItem = ItemList.get(OptionChosen -1);
+            User TransactionItemOwner = AvailableItems.get(TransactionItem);
+            // This next control flow accounts for if a user can make a transaction. DO I NEED TO DO THIS??
+            if(){
+                CreateTransactionMenu(scanner,TransactionItem,TransactionItemOwner);
+            }
+            else{
+
+            }
+        }
+
     }
-    private void SelectAvailableItem(Scanner scanner){
-        int nextOption = scanner.nextInt();
-        List<Item> AvailableItems = this.userMenu.getAvailableItems();
 
+
+    /**
+     * NOT FINISHED!
+     * This method handles the flow for setting up a transaction for an available item assuming that the transaction
+     * is allowed between the 2 users.
+     *
+     * @param scanner the scanner constructed in the run method.
+     * @param item The item that is going to be traded.
+     * @param Owner The other user that is currently the owner of the item you want to trade for.
+     */
+    private void CreateTransactionMenu(Scanner scanner, Item item, User Owner){
+        System.out.println("Transaction Menu");
+        System.out.println("----------------");
+
+        String FirstMeetingtitle ="First Meeting Details";
+        MeetingDetailsMenu(scanner, FirstMeetingtitle);
+        List<String> OptionList = new ArrayList<>();
+        OptionList.add("One Way Temporary Trade");
+        OptionList.add("One Way Permanent Trade");
+        OptionList.add("Two Way Temporary Trade");
+        OptionList.add("Two Way Permanent Trade");
+        String OptionPrompt = "Select the kind of trade you would like to make for this item?";
+        int OptionChosen = this.HandleOptions(scanner,OptionList,true,OptionPrompt);
+
+
+
+        int OneWayOption = scanner.nextInt();
+        if(OneWayOption == 1){
+            System.out.println("First Meeting Details");
+            System.out.println("----------------------");
+
+        }
 
 
 
     }
 
-    private void CreateTransaction(Scanner scanner){
-
+    /**
+     * NOT FINISHED!
+     *
+     * Question: Is it ok if I just ask sequentially about the meeting?(Even though this might not be extendable)
+     * @param scanner the scanner of the
+     * @param MeetingTitle The first thing that will be displayed "Second Meeting Details"/"First Meeting Details"
+     */
+    private void MeetingDetailsMenu(Scanner scanner, String MeetingTitle){
+        System.out.println(MeetingTitle);
+        System.out.println("----------------------");
+        System.out.println("What Location");
     }
-
 }
