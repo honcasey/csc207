4b459594ae5a19de5628e849c9d659a7ca48af55
Annie <56055607+annie951977@users.noreply.github.com>
2020-07-01 19:08:57 -0400

    - numWeeklyTransactions and its TransactionHistory Helper Method properly implemented

--- phase1/src/TransactionHistory.java
+++ phase1/src/TransactionHistory.java
@@ -13,8 +13,7 @@ public class TransactionHistory {
     private HashMap<LocalDate, Transaction> dateToTransaction; // decide if this is really needed
     private int numItemsLended = 0;
     private int numItemsBorrowed = 0;
-    // TODO: get week transactions --> this should be a boolean and this should call the threshold from the User
-    // TODO: toString id
+
 
     /**
      * Constructs an instance of TransactionHistory with an empty ArrayList transactions and an empty Hashtable usersNumTradeTimes
@@ -110,9 +109,18 @@ public class TransactionHistory {
     }
 
     /**
+     * @return an ArrayList of all Transactions
+     */
+    public ArrayList<Transaction>getAllTransactions(){
+        ArrayList<Transaction> allTransactions = new ArrayList<>();
+        allTransactions.addAll(oneWayTransactions);
+        allTransactions.addAll(twoWayTransactions);
+        return allTransactions;
+    }
+    /**
      * @return all of users and the times they have been traded with
      */
-    public  HashMap<User, Integer> getUsersNumTradeTimes(){
+    public HashMap<User, Integer> getUsersNumTradeTimes(){
         return usersNumTradeTimes;
     }
 
@@ -178,4 +186,31 @@ public class TransactionHistory {
 //       return newString.toString();
 //    }
 
+public int numTransactionsInWeek(){
+        int numTransactions = 0;
+        ArrayList<Transaction> allTransactions = getAllTransactions();
+//        ZoneId k = ZoneId.of("America/Montreal");
+//        LocalDate today = LocalDate.now(k); alternative way of doing it; saving just in case
+        Calendar currentCal = Calendar.getInstance();
+        int week = currentCal.get(Calendar.WEEK_OF_YEAR);
+        int year = currentCal.get(Calendar.YEAR);
+        Calendar targetCal = Calendar.getInstance();
+        int targetWeek;
+        int targetYear;
+        for (int i = 0; i < allTransactions.size();){
+            LocalDate date;
+            Transaction currTrans = allTransactions.get(i);
+            date = currTrans.getTransactionMeetings().get(currTrans.getTransactionMeetings().size() - 1).getDate();
+            Date setDate = java.sql.Date.valueOf(date);
+            targetCal.setTime(setDate);
+            targetWeek = targetCal.get(Calendar.WEEK_OF_YEAR);
+            targetYear = targetCal.get(Calendar.YEAR);
+            if(targetWeek == week && targetYear == year){
+                numTransactions ++;
+            }
+            i++;
+        }
+       // main idea of this code from: https://stackoverflow.com/questions/10313797/how-to-check-a-day-is-in-the-current-week-in-java
+        return numTransactions;
+   }
 }
