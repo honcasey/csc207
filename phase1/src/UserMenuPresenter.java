7f4a6f0cf0557666809157f792fcca27eaade515
Christian Mitrache <christian.mitrache@mail.utoronto.ca>
2020-07-04 11:57:29 -0400

    Made changes to handle options method. Broke it down to smaller methods that can be reused.
    Renamed some of the classes and created new UserMenuPresenter.java class

---
+++ phase1/src/UserMenuPresenter.java
@@ -0,0 +1,11 @@
+public class UserMenuPresenter {
+    /**
+     * This Presenter class contains helper methods that will be used to format and output the prompts that a user sees.
+     *
+     * Class variables representing common strings that users will see as prompts will be here.
+     *
+     * Methods that combine these common strings will be here.
+     *
+     * Methods that print output(Phase 1)/ display stuff(Phase 2) will be here.
+     */
+}
