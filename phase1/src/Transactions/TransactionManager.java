fd4c1ac13e1df8f6d2e09cb1dba32f9a4f223a6e
Anna Shirkalina <ashirkalina@gmail.com>
2020-07-09 22:36:13 -0400

    Added stuff into TransactionManager

--- phase1/src/Transactions/TransactionManager.java
+++ phase1/src/Transactions/TransactionManager.java
@@ -1,4 +1,61 @@
 package Transactions;
 
+import Exceptions.InvalidTransactionException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 public class TransactionManager {
+    private Map<UUID, Transaction> allTransactions;
+
+    public TransactionManager(Map<UUID, Transaction> transactions) {
+        allTransactions = transactions;
+    }
+
+    /**
+     * The getter that returns all of the Transactions across the whole system
+     * @return allTransactions, a HashMap<UUID, Transaction>
+     */
+    public Map<UUID, Transaction> getAllTransactions() {
+        return allTransactions;
+    }
+
+    public void removeTransactionFromAllTransactions(UUID id) throws InvalidTransactionException {
+        if (allTransactions.containsKey(id)){
+            allTransactions.remove(id);}
+        else {
+            throw new InvalidTransactionException();}
+    }
+
+    /**
+     * get the transaction from the list of all of the transactions by calling the id
+     * @param id the UUID of the transaction
+     * @return the transaction
+     */
+    public Transaction getTransactionFromId(UUID id) throws InvalidTransactionException {
+        if (allTransactions.containsKey(id)){
+            return allTransactions.get(id);}
+        else{
+            throw new InvalidTransactionException();
+        }
+    }
+
+
+    /**
+     * Put a list of ids and get an ArrayList of Transactions back
+     * @param transactionList : the list of transactions ids to be converted into Transactions
+     * @return An Array list of Transaction Objects
+     * @throws InvalidTransactionException the id's in the transactionList must match with the id's in AllTransactions
+     */
+    public ArrayList<Transaction> getTransactionsFromIdList(List<UUID> transactionList) throws InvalidTransactionException {
+        ArrayList<Transaction> transactions= new ArrayList<Transaction>();
+        for (UUID id : transactionList){
+            Transaction transaction = getTransactionFromId(id);
+            transactions.add(transaction);
+        }
+        return transactions;
+    }
+
 }
