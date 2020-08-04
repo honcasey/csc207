d03d072c9ea221cd3f71ba267b8200eaf0ba0eb5
Annie <anni.liu@mail.utoronto.ca>
2020-08-04 11:37:53 -0400

    - working on Upgrade Account window

--- phase2/src/DemoUserGUI/UpgradeAccountWindow.java
+++ phase2/src/DemoUserGUI/UpgradeAccountWindow.java
@@ -1,4 +1,39 @@
 package DemoUserGUI;
 
+import Initialization.PopUpWindow;
+import Users.DemoMenuController;
+import Users.UserMenuController;
+
+import javax.swing.*;
+import java.util.UUID;
+
 public class UpgradeAccountWindow {
+    private final JLabel userLabel = new JLabel("Username");
+    private final JTextField userText = new JTextField(20);
+    private final JLabel passwordLabel = new JLabel("Password");
+    private final JPasswordField passwordText = new JPasswordField(20);
+    private final JTextField confirmText = new JPasswordField(20);
+    private final JLabel confirmLabel = new JLabel("Confirm");
+    private final JButton createButton = new JButton("Sign up");
+    private final UserMenuController umc;
+    private final DemoMenuController dmc;
+
+    public UpgradeAccountWindow(DemoMenuController dmc, UserMenuController umc){
+        this.dmc = dmc;
+        this.umc = umc;
+    }
+    public void display(){
+
+    }
+
+    private void createAccount(String username, String password, String password2) {
+        if (umc.availableUsername(username) && password.equals(password2)) {
+            umc.addTradingUser(username, password);
+            UUID oldID = dmc.currentDemoUser.getUserId();
+            dmc.getDum().removeDemoUser(oldID);
+
+        } else {
+            new PopUpWindow("Username already taken! Please enter a new username.").display();
+        }
+    }
 }
--- phase2/src/Users/DemoMenuController.java
+++ phase2/src/Users/DemoMenuController.java
@@ -15,13 +15,8 @@ public class DemoMenuController {
         this.tum = tum;
         this.im = im;
     }
-    public void convertToTradingUser(DemoUser demoUser, String username, String password) throws InvalidTradingUserException {
-        tum.addTradingUser(username, password);
-        dum.removeDemoUser(demoUser.getUserId());
 
-        // TODO: need to figure out how to handle the case where the user wants to convert account to a Trading User one but the username is taken
+    public DemoUserManager getDum(){
+        return dum;
     }
-
-
-
 }
