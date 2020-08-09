dcf583ce126a8f4c8732f4d3ae333d4ea6f96cbb
Annie <anni.liu@mail.utoronto.ca>
2020-08-08 22:22:45 -0400

    - a single test

--- phase2/Test/DemoUserTest.java
+++ phase2/Test/DemoUserTest.java
@@ -1,2 +1,21 @@
+import org.junit.Test;
+import Users.DemoUser;
+import static org.junit.Assert.assertEquals;
+
 public class DemoUserTest {
+    @Test
+    public void demoUser(){
+        DemoUser bob = new DemoUser("bob", "1234");
+        assertEquals(bob.getUsername(), "bob");
+        assertEquals(bob.getPassword(), "1234");
+    }
+
+    @Test
+    public void demoUserItems(){
+        DemoUser bob = new DemoUser("bob", "1234");
+        assertEquals(bob.getInventory().size(),1);
+        assertEquals(bob.getWishlist().size(), 1);
+
+    }
+
 }
